7e2684fa2244fbbc79396b373dfc07551d2150e7
miyuu22 <139205386+miyuu22@users.noreply.github.com>
2024-01-19 23:15:15 +0900

    Delete db-connect.php

--- db-connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-    const SERVER = 'mysql219.phy.lolipop.lan';
-    const DBNAME = 'LAA1517802-hh';
-    const USER = 'LAA1517802';
-    const PASS = 'hunter';
-
-    $connect = 'mysql:host='. SERVER . ';dbname='. DBNAME . ';charset=utf8';
-?>
